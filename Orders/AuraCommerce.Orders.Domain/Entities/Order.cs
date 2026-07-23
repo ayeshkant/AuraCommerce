a78153d1598abf70fd48f9fa262c0def70b806a3
ayeshkant <ayeshkant.mishra7@gmail.com>
2026-07-23 22:00:36 +0530

    Added order details

---
+++ Orders/AuraCommerce.Orders.Domain/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuraCommerce.Orders.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public string CustomerId { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public decimal TotalAmount { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public OrderStatus Status { get; private set; }
+        public string IdempotencyKey { get; set; }
+
+        
+    }
+}
---
+++ Orders/AuraCommerce.Orders.Domain/Entities/OrderItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuraCommerce.Orders.Domain.Entities
+{
+    public class OrderItem
+    {
+        public Guid Id { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+    public enum OrderStatus
+    {
+        Created,
+        PaymentPending, 
+        Paid, 
+        Shipped, 
+        Cancelled
+    }
+}
