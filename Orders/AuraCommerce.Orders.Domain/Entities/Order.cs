edd8df2c65a391ec7fbddee80ad790972ebd44ec
ayeshkant <ayeshkant.mishra7@gmail.com>
2026-07-26 17:27:37 +0530

    Changes done

--- Orders/AuraCommerce.Orders.Domain/Entities/Order.cs
+++ Orders/AuraCommerce.Orders.Domain/Entities/Order.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace AuraCommerce.Orders.Domain.Entities
 {
     public class Order
     {
-        public Guid Id { get; set; }
-        public string CustomerId { get; set; }
-        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
-        public decimal TotalAmount { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public Guid Id { get; private set; }
+        public string CustomerId { get; private set; }
+        public List<OrderItem> Items { get; private set; } = new List<OrderItem>();
+        public decimal TotalAmount { get; private set; }
+        public DateTime CreatedDate { get; private set; }
         public OrderStatus Status { get; private set; }
-        public string IdempotencyKey { get; set; }
+        public string IdempotencyKey { get; private set; }
 
-        
+        private Order()
+        {
+            
+        }
+        static void Create()
+        {
+
+        }
     }
 }
