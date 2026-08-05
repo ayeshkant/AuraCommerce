31f3620c09a4349df188223950600df7385874de
ayeshkant <ayeshkant.mishra7@gmail.com>
2026-08-05 23:19:51 +0530

    Added IOrderRepository

---
+++ Orders/AuraCommerce.Orders.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using AuraCommerce.Orders.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuraCommerce.Orders.Domain.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Task<Order?> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<Order>> GetByCustomerIdAsync(string customerId);
+        Task<bool> ExistsWithIdempotencyKeyAsync(string idempotencyKey);
+        Task AddAsync(Order order);
+        Task SaveChangesAsync();
+    }
+}
