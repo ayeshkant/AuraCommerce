977ee1bb11ba7d91971944acc5ca5bf4bcf17057
ayeshkant <ayeshkant.mishra7@gmail.com>
2026-08-09 13:31:20 +0530

    Implemented phase A

---
+++ Orders/AuraCommerce.Orders.Application/DTOs/Result.cs
@@ -0,0 +1,19 @@
+﻿using AuraCommerce.Orders.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuraCommerce.Orders.Application.DTOs
+{
+    public class Result<T> : Result
+    {
+        public T Value { get; }
+        protected Result(bool isSuccess,T value, string? error) : base(isSuccess, error)
+        {
+            Value = value;
+        }
+
+        public static Result<T> Success(T value) => new(true, value, null);
+        public new static Result<T> Failure(string error) => new(false, default, error);
+    }
+}
---
+++ Orders/AuraCommerce.Orders.Application/Interface/ICatalogServiceClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuraCommerce.Orders.Application.Interface
+{
+    public interface ICatalogServiceClient
+    {
+        Task<ProductInfo?> GetProductAsync(string productId);
+    }
+
+    public record ProductInfo(string ProductId, string Name, decimal Price);
+}
