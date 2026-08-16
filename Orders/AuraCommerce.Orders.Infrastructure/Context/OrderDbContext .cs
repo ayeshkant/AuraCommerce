fda541aa1edd9faf94acbe43042866d7025d38c4
ayeshkant <ayeshkant.mishra7@gmail.com>
2026-08-16 22:57:05 +0530

    created db context

---
+++ Orders/AuraCommerce.Orders.Infrastructure/Context/OrderDbContext .cs
@@ -0,0 +1,31 @@
+﻿using AuraCommerce.Orders.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuraCommerce.Orders.Infrastructure.Context
+{
+    public class OrderDbContext:DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Order>(builder =>
+            {
+                // relationship + backing field configuration comes next
+            });
+
+            modelBuilder.Entity<OrderItem>(builder =>
+            {
+                // configuration comes next
+            });
+        }
+    }
+}
