07334465f7ae084eb176795e2b5374d424f59fa1
ayeshkant <ayeshkant.mishra7@gmail.com>
2026-08-30 17:00:55 +0530

    created catalog client

---
+++ Orders/AuraCommerce.Orders.Application/Client/CatalogHttpClient.cs
@@ -0,0 +1,32 @@
+﻿using AuraCommerce.Orders.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+
+namespace AuraCommerce.Orders.Application.Client
+{
+    public class CatalogHttpClient : ICatalogServiceClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public CatalogHttpClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<ProductInfo?> GetProductAsync(string productSku)
+        {
+            var response = await _httpClient.GetAsync($"/api/products/by-sku/{productSku}");
+
+            if (response.StatusCode==HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<ProductInfo>();
+        }
+    }
+}
