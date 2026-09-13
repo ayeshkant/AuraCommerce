8e5825f812d59f06f894e2dcedbaf0c2471b18e4
ayeshkant <ayeshkant.mishra7@gmail.com>
2026-09-13 16:46:19 +0530

    Added outbox message

---
+++ Orders/AuraCommerce.Orders.Infrastructure/Event/OutboxMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuraCommerce.Orders.Infrastructure.Event
+{
+    public class OutboxMessage
+    {
+        public Guid Id { get; private set; }
+        public string EventType { get; private set; }
+        public string Payload { get; private set; }
+        public DateTime OccurredOn { get; private set; }
+        public DateTime? ProcessedOn { get; private set; }
+        public OutboxMessage(string eventType, string payload, DateTime occurredOn)
+        {
+            Id = Guid.NewGuid();
+            EventType = eventType;
+            Payload = payload;
+            OccurredOn = occurredOn;
+        }
+        public void MarkAsProcessed()
+        {
+            ProcessedOn = DateTime.UtcNow;
+        }
+    }
+}
