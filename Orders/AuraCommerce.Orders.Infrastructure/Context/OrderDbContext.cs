9a1deffcd8b6dc1d3f3da4c076f9b0e1ba436e88
ayeshkant <ayeshkant.mishra7@gmail.com>
2026-09-13 17:38:49 +0530

    added save changes async

--- Orders/AuraCommerce.Orders.Infrastructure/Context/OrderDbContext.cs
+++ Orders/AuraCommerce.Orders.Infrastructure/Context/OrderDbContext.cs
@@ -1,8 +1,11 @@
 ﻿using AuraCommerce.Orders.Domain.Entities;
+using AuraCommerce.Orders.Domain.Interfaces;
+using AuraCommerce.Orders.Infrastructure.Event;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 
 namespace AuraCommerce.Orders.Infrastructure.Context
 {
@@ -13,7 +16,7 @@ namespace AuraCommerce.Orders.Infrastructure.Context
             
         }
         public DbSet<Order> Orders { get; set; }
-
+        public DbSet<OutboxMessage> OutboxMessages { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -30,6 +33,29 @@ namespace AuraCommerce.Orders.Infrastructure.Context
             {
                 builder.HasKey(oi => oi.Id);
             });
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var aggregatesWithEvents = ChangeTracker.Entries<AggregateRoot>()
+                .Select(a => a.Entity)
+                .Where(e => e.DomainEvents.Any())
+                .ToList();
+
+            foreach (var aggregate in aggregatesWithEvents)
+            {
+                foreach (var domainEvent in aggregate.DomainEvents)
+                {
+                    var outboxMessage = new OutboxMessage(
+                        domainEvent.GetType().Name,
+                        JsonSerializer.Serialize(domainEvent, domainEvent.GetType()),
+                        domainEvent.OccurredOn);
+
+                    OutboxMessages.Add(outboxMessage);
+                }
+                aggregate.ClearDomainEvents();
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
