c833756d1b6fb2ef9f935fe17507bc72a88ca2a0
ayeshkant <ayeshkant.mishra7@gmail.com>
2026-09-13 21:54:05 +0530

    changes for phase 2

--- Orders/AuraCommerce.Orders.Api/Program.cs
+++ Orders/AuraCommerce.Orders.Api/Program.cs
@@ -1,4 +1,6 @@
+using AuraCommerce.Orders.Application.Interfaces;
 using AuraCommerce.Orders.Domain.Interfaces;
+using AuraCommerce.Orders.Infrastructure.Client;
 using AuraCommerce.Orders.Infrastructure.Context;
 using AuraCommerce.Orders.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +12,10 @@ var connectionString = builder.Configuration.GetConnectionString("OrderDb");
 // Add services to the container.
 builder.Services.AddDbContext<OrderDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddHttpClient<ICatalogServiceClient, CatalogHttpClient>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["CatalogApi:BaseUrl"]!);
+}).AddStandardResilienceHandler();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
